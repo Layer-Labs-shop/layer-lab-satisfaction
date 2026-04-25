c01ec72f5e79ada89abe4dc659640ae6dddb9ba7
Layer-Labs-shop <layerlabscustomerservice@gmail.com>
2026-04-25 10:19:45 +0100

    Initialize Firebase and set up authentication

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyB2IfMpmlFidX4U8n9VMRRoZbiKDrPCEDU",
   authDomain: "layerlab-auth.firebaseapp.com",
@@ -7,3 +10,10 @@ const firebaseConfig = {
   appId: "1:148258695216:web:7cdeabcda3d3827316e5f5",
   measurementId: "G-EQDJDD9LTZ"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Auth
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
